ef377f19d539bc2548399a3122134fc02b80ab65
ArseniyXaoc <xaoc190789@gmail.com>
2021-07-30 21:22:29 +0300

    feat: styled button

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,7 +5,8 @@ import Raiting from './raiting/Raiting';
 function App() {
   return (
     <div className="App">
-      <Raiting raiting_number = {2.6} size_of_grade={17}/>
+      <header><Raiting raiting_number = {2.6} size_of_grade={17}/> <button className='button'>Написать отзыв</button> </header>
+      
     </div>
   );
 }
